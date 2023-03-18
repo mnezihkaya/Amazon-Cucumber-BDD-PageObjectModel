472468c8f1cd636dac6e265019b0bdd694bef69a
nezih <m.nezihkaya@gmail.com>
2023-03-17 20:53:45 -0400

    SearchButton tested

---
+++ src/test/java/com/StepDefinition/SearchButton.java
@@ -0,0 +1,35 @@
+package com.StepDefinition;
+
+import com.Pages.MainPage;
+import com.Utilities.BrowserUtilities;
+import com.Utilities.ConfigurationReader;
+import com.Utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class SearchButton {
+    MainPage mainPage=new MainPage();
+
+    @Given("user should on mainPage")
+    public void userShouldOnMainPage() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+    }
+    @And("verify title {string}")
+    public void verifyTitle(String title) {
+        BrowserUtilities.titleVerify(title);
+    }
+
+
+    @When("User click search area and write {string} and click submitButton")
+    public void userClickSearchAreaAndWriteAndClickSubmitButton(String text) {
+        BrowserUtilities.highlight(mainPage.searchBox);
+        mainPage.searchBox.click();
+        mainPage.searchBox.sendKeys(text);
+        BrowserUtilities.waitFor(5);
+        mainPage.submitButton.click();
+
+    }
+
+
+}
