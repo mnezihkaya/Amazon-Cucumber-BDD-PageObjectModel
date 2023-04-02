8793dcbc7b73943aa2b7f55b7b7de72b807f853e
nezih <m.nezihkaya@gmail.com>
2023-04-02 12:10:48 -0400

    MainPageMainModules

--- src/test/java/com/StepDefinition/MainPageMainModules.java
+++ src/test/java/com/StepDefinition/MainPageMainModules.java
@@ -2,13 +2,16 @@ package com.StepDefinition;
 
 import com.Pages.MainPage;
 import com.Utilities.BrowserUtilities;
+import com.Utilities.Driver;
 import io.cucumber.java.en.When;
 
-public class CustomerService {
+public class MainPageMainModules {
     MainPage mainPage=new MainPage();
 
     @When("User click {string}")
-    public void userClick(String module) {
-        MainPage.ChooseAndCLickMainPageModule(module);
+    public void userClick(String mainModule) {
+        MainPage.ChooseAndCLickMainPageModule(mainModule);
+
     }
+
 }
